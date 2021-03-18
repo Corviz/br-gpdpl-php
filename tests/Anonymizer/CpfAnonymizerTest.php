204cce8dee1da63dd855399fc86a61cd3bddd38e
Carlos <carloscarucce@gmail.com>
2021-03-18 15:55:51 -0300

    create CpfAnonymizerTest

---
+++ tests/Anonymizer/CpfAnonymizerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Anonymizer;
+
+use Corviz\BrGpdpl\Anonymizer\CpfAnonymizer;
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+class CpfAnonymizerTest extends TestCase
+{
+    public function testCpfMaskWithSeparators()
+    {
+        $this->assertEquals(
+            '111.***.***-**',
+            (new CpfAnonymizer("111.111.111-11"))->anonymized()
+        );
+    }
+
+    public function testCpfMaskWithoutSeparators()
+    {
+        $this->assertEquals(
+            '111********',
+            (new CpfAnonymizer("11111111111"))->anonymized()
+        );
+    }
+
+    public function testInvalidValue()
+    {
+        $this->expectException(Exception::class);
+        (new CpfAnonymizer('invalid'));
+    }
+}
