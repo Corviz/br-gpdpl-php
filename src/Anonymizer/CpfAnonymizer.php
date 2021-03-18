01c935c75bfad5637f4094d4011ddbd11afb65d7
Carlos <carloscarucce@gmail.com>
2021-03-18 15:55:34 -0300

    create CpfAnonymizer

---
+++ src/Anonymizer/CpfAnonymizer.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Corviz\BrGpdpl\Anonymizer;
+
+
+use Corviz\BrGpdpl\Anonymizer;
+
+class CpfAnonymizer extends Anonymizer
+{
+    /**
+     * @inheritDoc
+     */
+    protected static function getPattern(): string
+    {
+        return "/^([0-9]{3})(\\.?)[0-9]{3}(\\.?)[0-9]{3}(-?)[0-9]{2}$/";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function anonymize(string $original, string $pattern): string
+    {
+        return preg_replace($pattern, '$1$2***$3***$4**', $original);
+    }
+}
