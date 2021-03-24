4ff2f60277f23ddd31256992bddd21c3c5fe345f
Carlos <carloscarucce@gmail.com>
2021-03-24 14:56:57 -0300

    creating CreditCardNumberAnonymizer

---
+++ src/Anonymizer/CreditCardNumberAnonymizer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Corviz\BrGpdpl\Anonymizer;
+
+use Corviz\BrGpdpl\Anonymizer;
+
+class CreditCardNumberAnonymizer extends Anonymizer
+{
+
+    /**
+     * @inheritDoc
+     */
+    protected static function getPattern(): string
+    {
+        return '/^(\d{4}([ \-]?)\d{2})\d{2}([ \-]?)\d{4}([ \-]?\d{4})$/';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function anonymize(string $original, string $pattern): string
+    {
+        return preg_replace($pattern,'$1**$3****$4', $original);
+    }
+}
